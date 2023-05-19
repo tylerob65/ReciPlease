9a90d075a366b670a90ab5afc03c1162581d57fd
Tyler OBrien <tylerob65@gmail.com>
2023-05-19 02:29:20 -0400

    Passable Edit Recipe Skeleton

--- src/views/EditRecipe.jsx
+++ src/views/EditRecipe.jsx
@@ -1,119 +1,214 @@
-import { Box, Button, Container, Divider, Paper, TextField, Typography } from '@mui/material'
-import React, { useState } from 'react'
+import { Box, Button, Container, Divider, Grid, Paper, TextField, Typography } from '@mui/material'
+import React, { useState, Fragment } from 'react';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import { useMessage } from '../context/MessageContext'
+import InputAdornment from '@mui/material/InputAdornment';
+
 
 export default function EditRecipe() {
 
-    const [instructionsList, setInstructionsList] = useState([""])
+  // let { addMessage } = useMessage()
 
-    // const addInstruction
+  const [instructionsList, setInstructionsList] = useState(["1", "", ""])
+  const [ingredientsList, setIngredientsList] = useState(["2", "", ""])
 
-    const showInstructionInputs = () => {
+  const handleInstructionChange = (e, i) => {
+    const newInstructionsList = [...instructionsList]
+    newInstructionsList[i] = e.target.value
+    setInstructionsList(newInstructionsList)
+  }
 
-    }
+  const handleIngredientChange = (e, i) => {
+    const newIngredientsList = [...ingredientsList]
+    newIngredientsList[i] = e.target.value
+    setIngredientsList(newIngredientsList)
+  }
 
-    return (
-        <Container>
-            <br />
-            <Box
-                component="form"
-                sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
+  const handleAddInstruction = () => {
+    const newInstructionsList = [...instructionsList]
+    newInstructionsList.push("")
+    setInstructionsList(newInstructionsList)
+  }
+
+  const handleAddIngredient = () => {
+    const newIngredientsList = [...ingredientsList]
+    newIngredientsList.push("")
+    setIngredientsList(newIngredientsList)
+  }
+
+  const handleRemoveInstruction = () => {
+    const newInstructionsList = instructionsList.slice(0, -1)
+    setInstructionsList(newInstructionsList)
+
+  }
+
+  const handleRemoveIngredient = () => {
+    const newIngredientsList = ingredientsList.slice(0, -1)
+    setIngredientsList(newIngredientsList)
+  }
+
+  const showInstructionInputs = () => (
+    instructionsList.map((instruction, i) => (
+      <Fragment key={i}>
+        <Typography variant="h6" sx={{ mt: 1 }}>Step {i + 1}</Typography>
+        <TextField
+          label="Instruction"
+          id={"instruction" + i}
+          required
+          minRows={2}
+          multiline
+          fullWidth
+          value={instruction}
+          sx={{ my: 1 }}
+          onChange={(e) => handleInstructionChange(e, i)}
+        />
+      </Fragment>
+    ))
+  )
+
+  const showIngredientInputs = () => (
+    ingredientsList.map((ingredient, i) => (
+      <Fragment key={i}>
+        <Typography variant="h6" sx={{ mt: 1 }}>Ingredient {i + 1}</Typography>
+        <TextField
+          label="Ingredient"
+          id={"ingredient" + i}
+          required
+          fullWidth
+          value={ingredient}
+          sx={{ my: 1 }}
+          onChange={(e) => handleIngredientChange(e, i)}
+        />
+      </Fragment>
+    ))
+  )
+
+
+  return (
+    <Container>
+      <br />
+      <Box
+        component="form"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <Paper elevation={3} sx={{ p: 2 }}>
+
+          {/* 
+          RECIPE BASICS SECTION
+          */}
+
+          <Typography color="primary" variant="h4" mb={1}>Recipe Basics</Typography>
+          <TextField label="Recipe Title"
+            id="recipe-title"
+            fullWidth
+            required
+          />
+          <br /><br />
+
+          <TextField label="Image URL"
+            id="recipe-title"
+            required
+            fullWidth
+          />
+
+          <br/><br/>
+
+          <Box>
+          <TextField
+            label="Servings"
+            id="servings"
+            type="number"
+            sx={{width:"200px",mr:2,mb:2}}
+          />
+
+          <TextField
+            label="Cook Time"
+            id="cooktime"
+            type="number"
+            sx={{ width: "200px" }}
+            InputProps={{
+              endAdornment: <InputAdornment position='end'>mins</InputAdornment>
+
+            }}
+          />
+          </Box>
+
+          {/* IN PROGRESS ATTEMPT TO MESS AROUND WITH GRID */}
+
+          {/* <Grid container spacing={2} columns ={{xs:4,md:12}} justifyContent="center">
+            <Grid item xs={4} md={6} textAlign="center">
+              <TextField
+                label="Servings"
+                id="servings"
+                type="number"
+              // sx={{width:"200px"}}
+              />
+            </Grid>
+
+            <Grid item xs={4} md={6} textAlign="center">
+
+              <TextField
+                label="Cook Time"
+                id="cooktime"
+                type="number"
+                // sx={{ width: "200px" }}
+                sx={{flexGrow:1}}
+                InputProps={{
+                  endAdornment: <InputAdornment position='end'>mins</InputAdornment>
+
                 }}
-            >
-                <Paper elevation={3} sx={{p:2}}>
-                    <Typography variant="h4" mb={1}>Recipe Basics</Typography>
-                    <TextField label="Recipe Title"
-                        id="recipe-title"
-                        fullWidth
-                        required
-                        sx={{
-                            width: "md",
-                            flexGrow: 1
-                        }}
-                    />
-                    <br /><br />
-                    
-                    <TextField label="Image URL"
-                        id="recipe-title"
-                        required
-                        fullWidth
-                        sx={{
-                            width: "md",
-                            flexGrow: 1
-                        }}
-                    />
+              />
+            </Grid>
+          </Grid> */}
+          
 
-                    <Divider sx={{ my: 2 }} />
-                    
-                    <Typography variant="h4" mb={1}>Instructions</Typography>
-                    
-                    <Typography variant="h6" sx={{ mt: 1 }}>Step 1</Typography>
-                    <TextField
-                        label="Step 1 Instructions"
-                        id="testmultiline1"
-                        minRows={2}
-                        multiline
-                        fullWidth
-                        sx={{my:1}}
-                    />
+          <Divider sx={{ my: 2 }} />
 
-                    <Typography variant="h6" sx={{mt:1}}>Step 2</Typography>
-                    <TextField
-                        label="Step 2 Instructions"
-                        id="testmultiline2"
-                        minRows={2}
-                        multiline
-                        fullWidth
-                        sx={{ my: 1 }}
-                    />
-                    
-                    <Typography variant="h6" sx={{ mt: 1 }}>Step 3</Typography>
-                    <TextField
-                        label="Step 3 Instructions"
-                        id="testmultiline2"
-                        minRows={2}
-                        multiline
-                        fullWidth
-                        sx={{ my: 1 }}
-                    />
-                    <Box textAlign="center">
-                    <Button>Add New Step</Button>
-                    </Box>
-                </Paper>
-                
+          {/* 
+          INGREDIENTS SECTION
+          */}
 
-                <br />
-                
-                <br />
+          <Typography color="primary" variant="h4" mb={1}>Ingredients</Typography>
+          {showIngredientInputs()}
 
-                <Typography>Instructions</Typography>
-                <br />
-                
-                <br />
-                <Typography>Step 2</Typography>
-                <TextField
-                    id="testmultiline2"
-                    label="multiline"
-                    minRows={2}
-                    multiline
-                />
-                <br />
-                <Typography>Step 3</Typography>
-                <TextField
-                    id="testmultiline3"
-                    label="multiline"
-                    minRows={2}
-                    multiline
-                />
+          {/* Ingredients Buttons */}
+          <Box textAlign="center">
+            <Button sx={{ m: 1, width:"170px"}} variant="outlined" color="success" startIcon={<AddIcon />} onClick={handleAddIngredient} disabled={ingredientsList.length > 14}>
+              Add New Step
+            </Button>
 
+            <Button sx={{ m: 1, width:"170px"}} variant="outlined" color="error" startIcon={<RemoveIcon />} onClick={handleRemoveIngredient} disabled={ingredientsList.length <= 1}>
+              Remove Step
+            </Button>
+          </Box>
 
+          <Divider sx={{ my: 2 }} />
 
+          {/* 
+          INSTRUCTIONS SECTION
+           */}
 
+          <Typography color="primary" variant="h4" mb={1}>Instructions</Typography>
+          {showInstructionInputs()}
 
-                {/* </form> */}
-            </Box>
-        </Container>
-    )
+          {/* Instructions Buttons */}
+          <Box textAlign="center">
+            <Button sx={{ m: 1, width: "170px" }} variant="outlined" color="success" startIcon={<AddIcon />} onClick={handleAddInstruction} disabled={instructionsList.length > 14}>
+              Add New Step
+            </Button>
+
+            <Button sx={{ m: 1, width: "170px" }} variant="outlined" color="error" startIcon={<RemoveIcon />} onClick={handleRemoveInstruction} disabled={instructionsList.length <= 1}>
+              Remove Step
+            </Button>
+          </Box>
+
+        </Paper>
+      </Box>
+    </Container>
+  )
 }
