1899cb6054bc1affc5b101609d3adcebb08d62ac
Tyler OBrien <tylerob65@gmail.com>
2023-05-25 01:10:08 -0400

    Added Top Recipe Pagination

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -10,30 +10,33 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Pagination from '@mui/material/Pagination';
 import { Link } from 'react-router-dom';
+import LinearProgress from '@mui/material/LinearProgress';
+
 
 export default function Home() {
     const [recipes,setRecipes] = useState([])
+    const [maxPageCount, setMaxPageCount] = useState(5)
     const REACT_APP_BACKEND_URL_BASE = process.env.REACT_APP_BACKEND_URL_BASE
     const viewRecipeUrl = REACT_APP_BACKEND_URL_BASE + "/viewrecipe/"
 
-    const[page, setPage] = React.useState(1);
-    const handlePageChange = (event, value) => {
+    const[page, setPage] = useState(1);
+    const handlePageChange = async (event, value) => {
+        getRecipes(value)
         setPage(value);
     };
 
-    
-
-
-    const getRecipes = async () => {
-        const res = await fetch(REACT_APP_BACKEND_URL_BASE + "/getallrecipes")
+    const getRecipes = async (page_num) => {
+        const res = await fetch(REACT_APP_BACKEND_URL_BASE + "/gettoprecipes/" + page_num)
         const data = await res.json();
         if (data.status === 'ok') {
-            setRecipes(data.data)
+            setRecipes(data.data.recipe_list)
             console.log(data)
         }
+        setMaxPageCount(data.data.max_pages)
+        
     }
 
-    useEffect(()=>{getRecipes()},[])
+    useEffect(()=>{getRecipes(1)},[])
     
     return (
         <Container sx={{textAlign:"center"}}>
@@ -50,30 +53,37 @@ export default function Home() {
             >
                 Welcome
             </Typography>
-            <TableContainer component={Paper}>
+
+            <TableContainer elevation={4} component={Paper}>
                 <Table>
                     <TableHead>
                         <TableRow>
                             <TableCell ><b>Recipe Title</b></TableCell>
                             <TableCell ><b>Owner Username</b></TableCell>
+                            <TableCell sx={{textAlign:"center"}}><b>Likes Count</b></TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {recipes.map((recipeInfo, i) => (
                             <TableRow key={i} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                                 <TableCell>
-                                <Link to={"/viewrecipe/" + recipeInfo.id}>
-                                    {recipeInfo.title}
+                                <Link to={"/viewrecipe/" + recipeInfo.recipe_id}>
+                                    <Typography variant="a" color="secondary" sx={{ textDecoration: "underline" }}>
+                                    {recipeInfo.recipe_title}
+                                    </Typography>
                                 </Link>
                                 </TableCell>
                                 <TableCell>{recipeInfo.owner_username}</TableCell>
+                                <TableCell sx={{textAlign:"center"}}>{recipeInfo.like_count}</TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
                 </Table>
+            <Box display="flex" justifyContent="center" p={2}>
+            <Pagination count={maxPageCount} page={page} variant='outlined' color="secondary" onChange={handlePageChange} />
+            </Box>
             </TableContainer>
 
-            <Pagination count={10} page={page} onChange={handlePageChange} />
             {/* <Pagination count={10} page={1}/> */}
             <br />
 
@@ -82,7 +92,7 @@ export default function Home() {
             >
             </Box> */}
             {/* <img src="https://hips.hearstapps.com/hmg-prod/images/delish-202210-padseeew-165-1666972356.jpg?crop=1.00xw:0.752xh;0,0.144xh&resize=1200:*" alt="" /> */}
-
+            {/* <LinearProgress/> */}
         </Container>
     )
 }
