8e093fe741f767170deb405917218c9d91f21913
Tyler OBrien <tylerob65@gmail.com>
2023-05-31 20:26:49 -0400

    Added top users to Homepage

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -15,15 +15,22 @@ import HomeCarousel from '../components/HomeCarousel';
 
 export default function Home() {
   const [recipes, setRecipes] = useState([])
-  const [maxPageCount, setMaxPageCount] = useState(5)
+  const [topUsers, setTopUsers] = useState([])
+  const [maxPageCountRecipes, setMaxPageCountRecipes] = useState(5)
+  const [maxPageCountTopUsers, setMaxPageCountTopUsers] = useState(5)
   const REACT_APP_BACKEND_URL_BASE = process.env.REACT_APP_BACKEND_URL_BASE
-  const viewRecipeUrl = REACT_APP_BACKEND_URL_BASE + "/viewrecipe/"
 
-  const [page, setPage] = useState(1);
-  const handlePageChange = async (event, value) => {
+  const [mostLikesPage, setMostLikesPage] = useState(1);
+  const [topUserPage, setTopUserPage] = useState(1);
+  const handleMostLikesPageChange = async (event, value) => {
     getRecipes(value)
-    setPage(value);
+    setMostLikesPage(value);
   };
+
+  const handleTopUsersPageChange = async (event,value) => {
+    getTopUsers(value)
+    setTopUserPage(value)
+  }
 
   const getRecipes = async (page_num) => {
     const res = await fetch(REACT_APP_BACKEND_URL_BASE + "/gettoprecipes/" + page_num)
@@ -31,11 +38,19 @@ export default function Home() {
     if (data.status === 'ok') {
       setRecipes(data.data.recipe_list)
     }
-    setMaxPageCount(data.data.max_pages)
-
+    setMaxPageCountRecipes(data.data.max_pages)
   }
 
-  useEffect(() => { getRecipes(1) }, [])
+  const getTopUsers = async (page_num) => {
+    const res = await fetch(REACT_APP_BACKEND_URL_BASE + "/getusersbyrecipecount/" + page_num)
+    const data = await res.json();
+    if (data.status === 'ok') {
+      setTopUsers(data.data.user_list)
+    }
+    setMaxPageCountTopUsers(data.data.max_pages)
+  }
+
+  useEffect(() => { getRecipes(1);getTopUsers(1) }, [])
 
   return (
     <Container sx={{ textAlign: "center" }}>
@@ -52,7 +67,8 @@ export default function Home() {
         Welcome
       </Typography>
 
-      <br /><br />
+      <br />
+      
       <HomeCarousel />
       <br /><br />
 
@@ -63,9 +79,9 @@ export default function Home() {
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell ><b>Recipe Title</b></TableCell>
-              <TableCell ><b>Owner Username</b></TableCell>
-              <TableCell sx={{ textAlign: "center" }}><b>Likes Count</b></TableCell>
+              <TableCell ><Typography><b>Recipe Title</b></Typography></TableCell>
+              <TableCell ><Typography><b>Owner Username</b></Typography></TableCell>
+              <TableCell sx={{ textAlign: "center" }}><Typography><b>Likes Count</b></Typography></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -85,15 +101,59 @@ export default function Home() {
                     </Typography>
                   </Link>
                 </TableCell>
-                <TableCell sx={{ textAlign: "center" }}>{recipeInfo.like_count}</TableCell>
+                <TableCell sx={{ textAlign: "center" }}>
+                  <Typography>{recipeInfo.like_count}</Typography>
+                  </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
         <Box display="flex" justifyContent="center" p={2}>
-          <Pagination count={maxPageCount} page={page} variant='outlined' color="secondary" onChange={handlePageChange} />
+          <Pagination count={maxPageCountRecipes} page={mostLikesPage} variant='outlined' color="secondary" onChange={handleMostLikesPageChange} />
         </Box>
       </TableContainer>
+      <br />
+      <br />
+
+      {/* Users With Most Recipes */}
+      <TableContainer elevation={4} component={Paper}>
+        <br />
+        <Typography variant='h3'>Top Users</Typography>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <Typography sx={{ textAlign: "center" }}><b>User</b></Typography>
+                </TableCell>
+              <TableCell sx={{ textAlign: "center" }}>
+                <Typography><b>Recipe Count</b></Typography>
+                </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {topUsers.map((topUserInfo,i) => (
+              <TableRow key={i} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <TableCell sx={{ textAlign: "center" }}>
+                  <Link to={"/profile/" + topUserInfo.user_id}>
+                    <Typography variant="a" color="secondary" sx={{ textDecoration: "underline" }}>
+                      {topUserInfo.username}
+                    </Typography>
+                  </Link>
+                </TableCell>
+                <TableCell sx={{ textAlign: "center" }}>
+                  <Typography>{topUserInfo.recipe_count}</Typography>
+                  </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <Box display="flex" justifyContent="center" p={2}>
+          <Pagination count={maxPageCountTopUsers} page={topUserPage} variant='outlined' color="secondary" onChange={handleTopUsersPageChange} />
+        </Box>
+      </TableContainer>
+
+
+
 
       <br />
     </Container>
